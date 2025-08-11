7b5f38f1103b62334e73afbe95120b79fed58143
Neil Taylor <neiltaylor@uk.ibm.com>
2025-08-11 17:54:17 +0100

    Update boot_service.py

--- boot_service.py
+++ boot_service.py
@@ -331,7 +331,7 @@ sleep 2
 ntp time.adn.networklayer.com
 set base-url http://{Config.PXE_SERVER_DNS}:8080/boot/images
 
-kernel ${{base-url}}/kernel init=/bin/sh console=tty0 console=ttyS0,115200
+kernel ${{base-url}}/kernel init=/bin/sh console=tty0 console=ttyS0,115200 intel_iommu=off noapic acpi=ht pci=noacpi irqpoll processor.max_cstate=1
 initrd ${{base-url}}/initrd.img
 boot || goto error
 
